273ab8598f5e5dfcd4d2603fc001df054f3200c3
md rayhan munshi <jaber.ac1983@gmail.com>
2025-06-04 12:40:07 +0600

    if nested elif else statement rollercoaster project

--- python.py
+++ python.py
@@ -111,3 +111,20 @@
 #     print('This number is Even Number.')
 # else:
 #     print('This number is Odd Number.')
+
+
+########### if nested elif else statement
+print('Welcome to the rollercoaster!')
+height = int(input('Enter your height in cm: '))
+
+if height >= 120:
+    print('You can ride the rollercoaster.')
+    age = int(input("Enter your age: "))
+    if age < 12:
+        print('Please pay $5')
+    elif age <= 18:
+        print('Please pay $7')
+    else:
+        print('Please pay $12')
+else:
+    print('You are not allow to ride.')
