068a5413bf817c6b903a6a1d4b4b6f8d3e5928d2
md rayhan munshi <jaber.ac1983@gmail.com>
2025-06-10 09:45:53 +0600

    leap year calculate

--- python.py
+++ python.py
@@ -131,19 +131,35 @@
 
 
 
-
 ########### BMI Calculator 2.0
-height = float(input())
-weight = int(input())
+# height = float(input('Enter your height in Meeter: '))
+# weight = int(input('Enter your weight in kg: '))
 
-bmi = weight / (height * height)
-if bmi < 18.5:
-  print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi < 25:
-  print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi < 30:
-  print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif bmi < 35:
-  print(f"Your BMI is {bmi}, you are obese.")
+# bmi = weight / (height * height)
+# if bmi < 18.5:
+#   print(f"Your BMI is {bmi}, you are underweight.")
+# elif bmi < 25:
+#   print(f"Your BMI is {bmi}, you have a normal weight.")
+# elif bmi < 30:
+#   print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi < 35:
+#   print(f"Your BMI is {bmi}, you are obese.")
+# else:
+#   print(f"Your BMI is {bmi}, you are clinically obese.")
+
+
+
+########### Leap Year
+year = int(input('Write your carent year: '))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print('Leap Year')
+    else:
+      print('Not Leap Year')
+  else:
+    print('Leap Year')
 else:
-  print(f"Your BMI is {bmi}, you are clinically obese.")
+  print('Not Leap Year')
+
